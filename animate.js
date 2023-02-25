02e1ae73a290e38e98a594119918173aa6fa50a3
Boran Deniz Düzgün <91269287+PanDeniz@users.noreply.github.com>
2023-02-25 23:21:41 +0300

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,5 +1,4 @@
-
-
+ 
 $(".heart div").click(function(){
     $(".top").empty();
     $(".heart img").css("visibility", "visible");
@@ -8,22 +7,29 @@ $(".heart div").click(function(){
 
 $(".button1").click(function(){
     var x = Math.floor(Math.random()*101);
-    $(".middle div").css("visibility", "collapse");
-    $(".middle h4").text(`${x}%`).css({"color": "purple", "font-size": "1000%"}).fadeOut(200).fadeIn(200);
-    $(".middle div p").css("visibility", "visible");
-    $(".re").css("visibility", "visible");
+    $(".middle .colapseIt").css("visibility", "collapse");
+    $(".middle h4").text(`Calculating...`);
+    $("#calculate1").css({"visibility" :"visible", "position" : "relative"});
+    $("#calculate2").css({"visibility" :"visible", "position" : "relative"}).animate({width: "50%"},2000, function(){
+        $("#calculate1").css("visibility", "collapse");
+        $("#calculate2").css("visibility", "collapse");
+        $(".middle h4").text(`${x}%`).css({"color": "purple", "font-size": "650%"}).fadeOut(200).fadeIn(200);
+        $(".middle div p").css("visibility", "visible");
+        $(".re").css("visibility", "visible");
+    
+        if(x>=90){
+            $(".middle p").text("You are soul mates!!!");
+        }else if(x>=70){
+            $(".middle p").text("You are in love");
+        }else if(x>=40){
+            $(".middle p").text("You are like friends but nothing more");
+        }else if(x>=10){
+            $(".middle p").text("You can bareley resist each other");
+        }else{
+            $(".middle p").text("HATRED");
+        }
+    });
 
-    if(x>=90){
-        $(".middle p").text("You are soul mates!!!");
-    }else if(x>=70){
-        $(".middle p").text("You are in love");
-    }else if(x>=40){
-        $(".middle p").text("You are like friends but nothing more");
-    }else if(x>=10){
-        $(".middle p").text("You can bareley resist each other");
-    }else{
-        $(".middle p").text("HATRED");
-    }
 })
 
 $(".reload").click(function(){
